38f5d166b97086a61862546e3a1701e344ffcf9f
No6no6no7 <142609720+No6No6No7yt@users.noreply.github.com>
2025-09-17 09:49:58 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -5241,8 +5241,8 @@ function Library:CreateWindow(WindowInfo)
             New("ImageLabel", {
                 Image = (tonumber(WindowInfo.Icon) and{(string.format('rbxassetid://%s', tostring(WindowInfo.Icon)))}or{(WindowInfo.Icon)})[1],
                 Size = UDim2.fromOffset(64, 64),
-                Position = UDim2.fromOffset(8, 8),
-                AnchorPoint = Vector2.new(0, 0),
+                Position = UDim2.fromScale(0.5, 0.5),
+                AnchorPoint = Vector2.new(0.5, 0.5),
                 Parent = TitleHolder,
             })
         end
@@ -6622,3 +6622,4 @@ Library:GiveSignal(Teams.ChildRemoved:Connect(OnTeamChange))
 
 getgenv().Library = Library
 return Library
+--talua
